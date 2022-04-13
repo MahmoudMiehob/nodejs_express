d1ec3dc00e2d629977c3590e55711f210157dfc9
MahmoudMiehob <mahmoudmiehob@gmail.com>
2022-04-13 14:16:18 +0300

    modified app file

--- app.js
+++ app.js
@@ -4,3 +4,22 @@ const item = [1,[2,[3,[4]]]]
 
 const newitems = _.flattenDeep(item)
 console.log(newitems);
+
+const http = require ('http');
+
+const server = http.createServer((req,res)=>{
+    if(req.url === '/'){
+        res.end('end')
+    }
+    if(req.url === '/abc'){
+        res.end('abc')
+    }
+
+    // res.write('hello world');
+    res.end(`
+    <h1>opps !!!</h1>
+    `)
+})
+
+
+server.listen(5000)
